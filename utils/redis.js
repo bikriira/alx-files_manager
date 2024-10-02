c40a9e1355f93dcec1958af4a54a5fdcd643d79e
simowardi <daoist32@gmail.com>
2024-10-02 15:06:02 -0700

    update all

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,7 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
 
+
 /**
  * A class representing a Redis client.
  */
@@ -8,54 +9,33 @@ class RedisClient {
   /**
    * Initializes a new instance of RedisClient.
    */
+class RedisClient {
   constructor() {
     this.client = createClient();
-    this.connectionActive = true;
+    this.isClientConnected = true;
 
     this.client.on('error', (err) => {
       console.error('Redis client failed to connect:', err.message || err.toString());
-      this.connectionActive = false;
+      this.isClientConnected = false;
     });
-
     this.client.on('connect', () => {
-      this.connectionActive = true;
+      this.isClientConnected = true;
     });
   }
 
-  /**
-   * Checks whether the connection to the Redis server is active.
-   * @returns {boolean}
-   */
-  isConnected() {
-    return this.connectionActive;
+  isAlive() {
+    return this.isClientConnected;
   }
 
-  /**
-   * Fetches the value associated with a specified key.
-   * @param {String} key The key for the item to retrieve.
-   * @returns {String | Object}
-   */
   async get(key) {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  /**
-   * Saves a key-value pair with an expiration duration.
-   * @param {String} key The key for the item to save.
-   * @param {String | Number | Boolean} value The value to store.
-   * @param {Number} duration The expiration time in seconds.
-   * @returns {Promise<void>}
-   */
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
       .bind(this.client)(key, duration, value);
   }
 
-  /**
-   * Deletes the value associated with a specified key.
-   * @param {String} key The key for the item to delete.
-   * @returns {Promise<void>}
-   */
   async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
@@ -63,3 +43,4 @@ class RedisClient {
 
 export const redisClient = new RedisClient();
 export default redisClient;
+
